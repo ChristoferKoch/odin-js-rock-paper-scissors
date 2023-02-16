7ffd4e912b95e30657880929c80e17c78a9d088c
Christofer Koch <cbkoch13@gmail.com>
2023-02-15 22:27:07 -0800

    Add playRound()

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function getComputerChoice() {
-    let selection = Math.floor(Math.random() * 3);
+    let selection = Math.floor(Math.random() * 3); // Get random number between 0 and 2
 
     if (selection == 0) {
         selection = "Rock";
@@ -11,3 +11,37 @@ function getComputerChoice() {
 
     return selection;
 }
+
+function playRound(playerSelection, computerSelection) {
+    let win = "You win! " + playerSelection + " beats " + computerSelection + "!";
+    let lose = "You lose! " + playerSelection + " beats " + computerSelection + "!";
+
+    if (playerSelection === computerSelection) {
+        return "Draw!";
+    } else {
+        if (playerSelection === "Rock") {
+            if (computerSelection === "Paper") {
+                return lose;
+            } else {
+                return win;
+            }
+        } else if (playerSelection === "Paper") {
+            if (computerSelection === "Scissors") {
+                return lose;
+            } else {
+                return win;
+            }
+        } else {
+            if (computerSelection === "Rock") {
+                return lose;
+            } else {
+                return win;
+            }
+        }
+    }
+}
+
+const playerSelection = "Rock";
+const computerSelection = getComputerChoice();
+
+console.log(playRound(playerSelection,computerSelection));
